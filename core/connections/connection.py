ff1a5c1a320d12c4a3d9a045be27e6877b950ecc
Arturo Negreiros <anegreiross@outlook.com>
2021-06-27 23:58:06 -0500

    core and keys defined

--- core/connections/connection.py
+++ core/connections/connection.py
@@ -1,6 +1,10 @@
-from core.keys import API_KEY
+from core.keys import *
+import json
+from ibm_watson import *
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 
-print(API_KEY)
 
+ibm_authenticator = IAMAuthenticator(API_KEY, url = URL)
+discovery = Dicovery
 
--- core/keys.py
+++ core/keys.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-:constants
-"""
 
 API_KEY = "994J2O-bTozutuNika8YpvjWhHcAbGL5LifeONEnRIr1"
 URL = "https://api.us-south.assistant.watson.cloud.ibm.com/instances/68c3b95a-97d1-46ee-9a68-5f1dfae1cc7b"
