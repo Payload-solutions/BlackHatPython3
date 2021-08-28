87b4e960cc4b949209efa0bcc814d341bbae9ba7
Arturo <pxyload@localhost.localdomain>
2021-08-27 23:49:54 -0500

    created chapter two about socket and connections

--- core/keys.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-
-API_KEY = "994J2O-bTozutuNika8YpvjWhHcAbGL5LifeONEnRIr1"
-URL = "https://api.us-south.assistant.watson.cloud.ibm.com/instances/68c3b95a-97d1-46ee-9a68-5f1dfae1cc7b"
-ASSISTANT_ID = "6d903aef-3a4f-4ce5-b9db-4de365fb7675"
