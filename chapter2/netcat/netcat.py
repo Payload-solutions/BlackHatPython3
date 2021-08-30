f9725d7a7ee2eba37bc01417effeea2ba5b97aad
Arturo Negreiros <anegreiross@outlook.com>
2021-08-29 20:04:11 -0500

    updated because, the lights out

--- chapter2/netcat/netcat.py
+++ chapter2/netcat/netcat.py
@@ -1,5 +1,4 @@
 
-
 import threading
 import shlex
 import argparse
@@ -7,6 +6,9 @@ import subprocess
 import sys
 import textwrap
 import socket
+
+# data type
+socket_obj = socket.socket
 
 
 class NetCat:
@@ -18,14 +20,15 @@ class NetCat:
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR , 1)
 
     def run(self):
-
         if self.args.listen:
             self.listen()
         else:
             self.send()
 
     def send(self):
-
+        """We connect to the target and port and if we have a buffer, we send to 
+        the target first, otherwise if no more inputs, broke the loop
+        """
         self.socket.connect((self.args.target, self.args.port))
         if self.buffer:
             self.socket.send(self.buffer)
@@ -53,5 +56,24 @@ class NetCat:
 
 
 
+    def listen(self):
+        """When the program run as listener
+        That's implicate using the another method for socket connections
+        the bind method, and start to listening in a loop
+        """
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+
+        while True:
+            client_socket, _ = self.socket.accept()
+
+            socket_thread = threading.Thread(target=self.handler, args=(client_socket,))
+            socket_thread.start()
+
+
+    def handler(self, client_socket: socket_obj):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
 
 
