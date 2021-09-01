c9ed1a872a190498cb949853e6cd61cdbaf83276
Arturo Negreiros <anegreiross@outlook.com>
2021-09-01 15:02:58 -0500

    mail sniffer

---
+++ chapter4/mail_sniffer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from scapy.all import sniff
+
+
+def packet_callback(packet):
+    print(packet.show())
+
+
+
+def main():
+    sniff(prn=packet_callback, count=1)
+
+
+if __name__ == "__main__":
+    main()
