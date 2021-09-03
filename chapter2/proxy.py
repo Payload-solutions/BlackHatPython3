ebb02a562ecbc269be7439551acbfd7b02aea34f
Arturo Negreiros <anegreiross@outlook.com>
2021-09-03 00:55:12 -0500

    50% of proxy server completed

--- chapter2/proxy.py
+++ chapter2/proxy.py
@@ -71,6 +71,76 @@ def receive_from(connection: socket.socket):
 
 
 
+"""Inside these functions, uou can modify the packet contents, perform fuzzing tasks, test for
+authentication issues, or fo whatever else your heart desires. This can be useful, for example, 
+if you find plaintext user credentials sent and want to try to elevate privileges on an application
+by passing in admin inestead of your own username"""
+def request_handler(buffer):
+    # perform packet moddifications
+    return buffer
+
+def response_handler(buffer):
+    # perform packet modifications
+    return buffer
+
+
+def proxy_handler(client_socket: socket.socket, remote_host: str, remote_port: int, receive_first):
+    """The function contains the bulk of the logic for our proxy. To start off, we connect to the
+    remote host. Then we check to make sure we don't need to first initiate a connection to the remote
+    side and request data before going into the main loop. Some server daemons will expect you to do
+    this (FTP servers typically send a banner first, for example). We then use the receive_from func-
+    tion for both sides of the communication. It accepts a connected socket object and performs a receive.
+    We dump the contents of the packet so that we can inspect it for anything interesting. Next, we hand
+    the output to the response_handler function and then send the received buffer to the local client.
+    The rest of the proxy code is straightforward: we set up our loop to continually read from the local
+    client, process the data send it to the remote client, read from the remote client, process the 
+    data, and send it to the local client until w no longer detect any data. when there's no dat to send
+    on either side of the connection, we close both the local and remote sockets and break out of the loop.
+
+    Let's put together the sever_loop function to set up and manage the connection"""
+    remote_socket = socket.socket(socket.AF_INTE, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+
+
+    remote_buffer = response_handler(remote_buffer)
+
+    if len(remote_buffer):
+        print("[<==] Sending %d bytes to localhosts."%len(remote_buffer))
+        client_socket.send(remote_buffer)
+
+
+    while True:
+        local_buffer = receive_from(client_socket)
+        if len(local_buffer):
+            line = "[==>]Received %d bytes from localhost."%len(local_buffer)
+
+            print(line)
+            hexdump(local_buffer)
+
+            local_buffer = request_handler(local_buffer)
+            remote_socket.send(local_buffer)
+            print("[==> Send to remote.]")
+
+        remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print("[<==] Received %d bytes from remote. "%len(remote_buffer))
+            hexdump(remote_buffer)
+
+            remote_buffer = response_handler(remote_buffer)
+            client_socket.send(remote_buffer)
+            print("[<==] Sent to localhost.")
+
+        if not len(local_buffer) or not len(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            print("[*] No more data. Closing connecions.")
+            break
+
+
 
 def main():
     pass
