53d7f1a8f7a23d71a24f783fdaaeaf79cdef7173
Arturo Negreiros <anegreiross@outlook.com>
2021-09-05 12:59:31 -0500

    building a ssh tuneling ok

---
+++ chapter2/ssh_cmd.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+#coding: utf-8
+
+
+import paramiko
+from typing import Any
+
+
+
+
+def ssh_command(ip: str, port: int, user: str, passwd: str, cmd: Any) -> None:
+
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, port=port, username=user, password=passwd)
+
+    _, stdout, stderr = client.exec_command(cmd)
+    output = stdout.readlines() + stderr.readlines()
+
+    if output:
+        print("--- Output ---")
+        for line in output:
+            print(line.strip())
+
+
+def main():
+    import getpass
+    # user = getpass.getuser()
+    user = input("Username: ")
+    password = getpass.getpass()
+
+    ip = input("Enter server ip: ") or "192.168.100.8"
+    port = input("Enter port or <CR>: ") or 2222
+    cmd = input("Enter command or <CR>: ") or "id"
+    ssh_command(ip, port, user, password, cmd)
+
+
+if __name__ == "__main__":
+    main()
