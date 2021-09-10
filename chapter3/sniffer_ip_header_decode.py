2ff2697a532cf571edcba0fa38427e41267c15b3
Arturo Negreiros <anegreiross@outlook.com>
2021-09-09 23:16:49 -0500

    ok.

--- chapter3/sniffer_ip_header_decode.py
+++ chapter3/sniffer_ip_header_decode.py
@@ -40,41 +40,41 @@ class IP:
         self.protocol = str(self.protocol_num)
     
 
-    def sniff(host):
-        # should look familiar from previous example
-        if os.name == "nt":
-            socket_protocol = socket.IPPROTO_IP
-        else:
-            socket_protocol = socket.IPPROTO_ICMP
-        
-        sniffer = socket.socket(socket.AF_INET, 
-                                socket.SOCK_RAW, socket_protocol)
-        sniffer.bind((host, 0))
-        sniffer.setsockopt(socket.ipproto_ip, socket.IP_HDRINCL, 1)
+def sniff(host):
+    # should look familiar from previous example
+    if os.name == "nt":
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+    
+    sniffer = socket.socket(socket.AF_INET, 
+                            socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((host, 0))
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
+    if os.name == "nt":
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    
+    try:
+        while True:
+            # read a packet
+            raw_buffer = sniffer.recvfrom(65535)[0]
+            # create an IP header from the first 20 bytes
+            ip_header = IP(raw_buffer[0:20])
+            # print the detected protocol and hosts
+            print(f"Protocol: {ip_header.protocol} {ip_header.src_address} {ip_header.dst_address}")
+    except KeyboardInterrupt:
+        # if we're on windows, turn off promiscuous mode
         if os.name == "nt":
-            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-        
-        try:
-            while True:
-                # read a packet
-                raw_buffer = sniffer.recvfrom(65535)[0]
-                # create an IP header from the first 20 bytes
-                ip_header = IP(raw_buffer[0:20])
-                # print the detected protocol and hosts
-                print(f"Protocol: {ip_header.protocol} {ip_header.src_address} {ip_header.dst_address}")
-        except KeyboardInterrupt:
-            # if we're on windows, turn off promiscuous mode
-            if os.name == "nt":
-                sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
-            sys.exit()
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        sys.exit()
 
 def main():
     
     if len(sys.argv) == 2:
         host = sys.argv[1]
     else:
-        host = "192.168.100.8"
+        host = "192.168.100.75"
     
     sniff(host)
 
